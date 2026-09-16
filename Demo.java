b1328cb6b80a532e7ebf8eae3615b4db4e8e7820
Sandip Mane <manesandip420@gmail.com>
2026-09-16 23:10:23 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,12 @@
+class Student{
+    String name = "Sandip";
+    int age = 21;
+}
+public class Demo {
+    public static void main(String[]args)
+    {
+        Student st = new Student();
+        System.out.println("Name " + st.name);
+        System.out.println("Age " + st.age);
+    }
+}
